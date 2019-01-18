088571964f5e27503216ba2ed1013ed89541e278
Jason Smythe <jason@smythemail.za.net>
2019-01-18 21:38:23 +0100

    Test adding image to PR comment

--- demo.js
+++ demo.js
@@ -5,4 +5,7 @@ const bot = require(".").create();
 bot.comment(process.env.GH_AUTH_TOKEN, `
 <h3>${bot.commitMessage()}</h3>
 Demo: <strong>${bot.artifactLink('demo/index.html', 'demo')}</strong>
+
+With a test image:
+![TEST](${bot.artifactUrl('demo/test-image.png', 'demo')})
 `);
